efb90fc68363df4cb481ca94efde5f9e72079cf5
Ayaan Abbasi <34920479+mayaanabbasi@users.noreply.github.com>
2020-10-10 18:54:40 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const path = require("path")
+const express = require("express")
+
+const app = new express()
+
+app.use(express.static("public"))
+
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname,"pages/index.html"))
+})
+
+app.listen(3000, () => {
+    console.log("App listening to port 3000")
+})
